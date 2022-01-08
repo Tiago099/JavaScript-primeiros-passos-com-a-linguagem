dea85e8d5d8b2ea1a6ad12dc11c7ae176fc52eb8
Tiago099 <tiagosebastian99@gmail.com>
2022-01-08 19:00:12 -0300

    Aprendendo a trabalhar com NodeJs e JavaScript

---
+++ Js/1-primeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log ("primeiro programa vamos trabalha com variaveis");
+
+const idade = 30;
+
+console.log (idade);
+console.log (idade+2);
+console.log (idade-2);
+console.log (idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
