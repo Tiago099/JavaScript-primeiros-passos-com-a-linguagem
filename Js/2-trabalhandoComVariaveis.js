2ec1ecc85266a8bcd94ee7d60c061e9512ce1b9e
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-10 22:47:11 -0300

    Tipos de Variáveis

---
+++ Js/2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+console.log("trabalhando com variaveis");
+//JS é case sensetive
+
+
+const idade = 29;
+const nome = "Tiago";
+
+console.log(idade+2); 
+console.log(nome);
+
+//não fazer
+//mes = "Janeiro";
---
+++ Js/5-atribuicaoVariaveis.js
@@ -0,0 +1,16 @@
+console.log("trabalhando com atribuição de variáveis");
+const idade = 30;
+const primeiroNome = "Tiago";
+const sobrenome = "Sebastian";
+
+
+//console.log(nome + " " + sobrenome) =
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+ const nomeCompleto = primeiroNome + " " + sobrenome;
+console.log(nomeCompleto);
+
+
+
+
